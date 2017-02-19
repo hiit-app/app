3a288280f04e2a7cec2f4e152d23a59b1126f87c
hiit-app <propiedades.hsierra@gmail.com>
2017-02-19 17:44:14 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@ var CACHED_URLS = [
      "sw_app.js",
      "percepcion.php",
      "header.php",
-        "haciendoheader.php",
+        
         "manifest.json",
         "bootstrap-3.3.7-dist/js/bootstrap.min.js",
         "bootstrap-3.3.7-dist/js/bootstrap.js",
